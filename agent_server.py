5fa91033f7fff400f13daeb18da28b856db0023e
Yanick Larose <57417735+ylarose@users.noreply.github.com>
2025-09-04 16:23:39 +0200

    Adding files for HTML
    Adding a file including an agent as a server, a small HTTP server, and a simple HTML interface

---
+++ agent_server.py
@@ -0,0 +1,91 @@
+# implements a LangChain agent as a server. To be used by a webserver
+
+import os
+from datetime import datetime
+
+from langchain.chat_models import init_chat_model
+
+from langchain_core.runnables import RunnableSequence
+from langchain_core.messages import HumanMessage
+
+from langchain.globals import set_debug
+
+from langgraph.prebuilt import create_react_agent
+from langgraph.checkpoint.memory import InMemorySaver
+
+from bedrock_ai import init_model
+
+from mytools import createTicket
+
+# giving general instructions to the model
+INSTRUCTION_GB = """Your name is Bob, you speak english, your are helping customers having issues with their iPhones
+    The only iPhone types supported by you are: iPhone 14 and iPhone 15.
+    If the customer's iPhone type is different, apologize and tell them you will forward their request to an agent.
+    If the customer's iPhone type is 14 or 15, ask them if the iPhone is damaged or lost.
+    if the customer iPhone is damaged or lost, ask for the reference number, found in the email they have received.
+    Finally, create a ticket using the createTicket tool, and give the customer the ticket number returned by the tool.
+    To end conversation, whish the customer a very happy day.
+    """
+
+INSTRUCTION_FR = """Votre nom est Jean, vous parlez et comprenez le francais, et vous etes un agent qui aid eles clients ayant des problèmes avec leurs iPhone.
+    Les seuls modeles supportés sont les iPhone 14 et 15.
+    Si le client a un iPhone 14 ou 15, il faut leur demander si leur iPhone est perdu ou abimé.
+    Si le client a un iPhone qui n'est pas un model 14 ou 15, propsez leur de parler a un agent
+    Si le client a un iPhone 14 ou 15, abimé ou perdu, il faut créer un ticket par l'outil createTicket, en passant le type d'iPhone 
+    et la référence de l'iPhone. Le client peut trouver la référence dans l'email qui lui a été envoyé lors de l'achat
+    Pour finir, souhaitez une bonne journée au client, en le remerciant de vous avoir contacté
+    """
+
+CONFIG = {"configurable": {"thread_id": "1"}}
+
+
+# test a question and answer agent
+def init_agent(model, instructions):
+    resp = None
+    
+    # memory pointer
+    memory = InMemorySaver()
+
+    # list of tools available to agent
+    tools = [createTicket]
+    
+    # create agent with model and tool list
+    agent = create_react_agent(model, tools, checkpointer=memory)
+
+    resp = ""
+    for chunk in agent.stream({"messages": [HumanMessage(content=instructions)]}, CONFIG):
+        # if the data comes from agent or tools, the text is in a different place
+        if 'agent' in chunk.keys():
+            data = chunk['agent']['messages'][0].text()
+        elif 'tools' in chunk.keys():
+            data = chunk['tools']['messages'][0].text()
+
+        resp = resp + data  
+    
+    return agent, resp
+
+
+# test a question and answer agent
+def ask_question(agent, question):
+    resp = ""
+    
+    for chunk in agent.stream({"messages": [HumanMessage(content=question)]}, CONFIG):
+        resp = resp + chunk['agent']['messages'][0].text()
+    
+    return resp 
+
+### END OF CODE
+
+
+### unit testing
+def test_server():
+    model = init_model()
+    agent, firstAnswer = init_agent(model, INSTRUCTION_GB)
+    print(f"answer: {firstAnswer}")
+
+    question = "Hi, my name is Axel, I just lost my iPhone"
+    print(f"question: {question}")
+    answer = ask_question(agent, question)
+    print(f"answer: {answer}")
+
+test_server()
---
+++ https.py
@@ -0,0 +1,102 @@
+# Simple HTTP Server. does not support https
+# needs python3 and Flask library
+# serves all supportTools webservices (/getDRGDetails, /getConversation, ...)
+
+# to test offline, uncomment lines with after: "for offline tests"
+
+from flask import Flask, render_template, request, send_from_directory, jsonify, abort
+
+import sys
+import time
+from datetime import datetime, timedelta
+from bedrock_ai import init_model
+from agent_server import init_agent, ask_question
+
+app = Flask(__name__)
+
+app.Debug = True
+
+# global variable to store agent
+G_AGENT = None
+
+def log(text):
+    now = datetime.now()
+    strDT = now.strftime("%Y-%m-%d, %H:%M:%S")
+    print(f"{strDT} - {text}")
+
+# path to files to parse
+
+@app.route('/')
+def index():
+    log('Got /')
+    return 'Hello world!'
+
+
+@app.route('/<path:path>')
+def getFile(path):
+    # only accept extension in: .html, .js, .jpg
+    log('Getting file: ' + path)
+    str = ""
+    if path.endswith(".html") or path.endswith(".js") or path.endswith(".jpg") or path.endswith(".svg") or path.endswith(".mp3") or path.endswith(".gif") or path.endswith(".ico") or path.endswith(".css") or path.endswith(".png"):
+        str = send_from_directory('', path)
+    else:
+        log('File type not supported: ' + path)
+        # awslogger.addlog('Getting file: ' + path)
+        str = "<h2>File not supported</h2>"
+
+    return str
+
+# setup agent and LLM, returns first question
+@app.route('/setupagent', methods=['GET', 'POST'])
+def setupAgent():
+    global G_AGENT
+
+    log('In /setupAgent')
+    jsdata = None
+    
+    instructions = request.form.get('instructions')
+    log("instructions: %s" % (instructions))
+  
+    # init LLM model
+    model = init_model()
+
+    # now, init agent with instructions
+    agent, response = init_agent(model, instructions)
+    G_AGENT = agent
+    log(f"got answer {response}")
+    
+    jsdata = jsonify(response) 
+        
+    return jsdata
+
+
+# get details of conversation, with server, convID, date, time
+@app.route('/askquestion', methods=['GET', 'POST'])
+def askQuestion():
+    global G_AGENT
+    
+    log('In /askQuestion')
+    
+    question = request.form.get('question')
+    
+    log(f"question: {question}")    
+    response = ask_question(G_AGENT, question)
+    log(f"response: {response}")
+    
+    jsdata = jsonify(response) 
+        
+    return jsdata
+
+
+if __name__ == '__main__':
+  
+    # print('Argument List:', str(sys.argv))
+    if (len(sys.argv) > 1):
+        globalPath = sys.argv[1]
+
+    # app.logger.setLevel(logging.INFO)
+    log("Starting httpserver.py")
+    
+    app.run(debug=True, host='0.0.0.0', port=8080)
+
+
